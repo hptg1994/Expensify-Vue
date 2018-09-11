080967dd426d3e40b0697d13789c4d74b08ef873
hptg1994 <hptg1994@gmail.com>
2018-09-11 18:01:18 -0400

    adding the vuex persist

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,17 @@
 import vuex from "vuex"
 import Vue from "vue";
 import expense from "./expense/expense";
+import VuexPersist from 'vuex-persist'
 
 Vue.use(vuex);
 
+const vuexPersist = new VuexPersist({
+  key: 'expensify',
+  storage: localStorage
+})
+
 export const store = new vuex.Store({
+  plugins: [vuexPersist.plugin],
   modules:{
     expense
   }
